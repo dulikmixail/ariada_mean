1f31eeb2d6123e26236689a290511dbb18c469a1
Mikhail Dulik <dulikmixail@gmail.com>
2019-03-14 18:03:35 +0300

    [14.03.2019]

--- src/app/_helpers/form.ts
+++ src/app/_helpers/form.ts
@@ -4,6 +4,7 @@ import {FormGroupConverter} from './form-group-converter';
 import {Store} from '@ngrx/store';
 import {AppState} from '../store';
 import {MatSnackBar} from '@angular/material';
+import {environment} from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -19,13 +20,14 @@ export class Form {
   submit(formDirective: FormGroupDirective, form: FormGroup, storeActionClass): Promise<boolean> {
     return new Promise<boolean>((resolve, reject) => {
       if (form.invalid) {
-        this.snackBar.open();
+        this.snackBar.open(environment.info['2'], environment.info['1']);
         reject(false);
       } else {
         const fd = this.formGroupConverter.load(form).toFormData();
         this.store.dispatch(new storeActionClass(fd));
         formDirective.resetForm();
         form.reset();
+        this.snackBar.open(environment.errors['5'], environment.errors['1']);
         resolve(true);
       }
     });
